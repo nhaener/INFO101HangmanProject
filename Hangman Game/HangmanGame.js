510e36fea9c115f9ea4a78d7659b72865a7ded7f
Nicholas Haener <nhaener@uw.edu>
2016-04-28 13:01:03 -0700

    game almost finished

--- Hangman Game/HangmanGame.js
+++ Hangman Game/HangmanGame.js
@@ -37,17 +37,6 @@ window.onload = function() {
     }
   }
 
-  // Select Catagory
-  var selectCat = function() {
-    if (chosenCategory === categories[0]) {
-      catagoryName.innerHTML = "The Chosen Category Is Premier League Football Teams";
-    } else if (chosenCategory === categories[1]) {
-      catagoryName.innerHTML = "The Chosen Category Is Films";
-    } else if (chosenCategory === categories[2]) {
-      catagoryName.innerHTML = "The Chosen Category Is Cities";
-    }
-  }
-
   // Create geusses ul
   result = function() {
     wordHolder = document.getElementById('hold');
@@ -75,6 +64,7 @@ window.onload = function() {
     showLives.innerHTML = "You have " + lives + " lives";
     if (lives < 1) {
       showLives.innerHTML = "Game Over";
+      showLives.innerHTML = "The answer was: " + word;
     }
     for (var i = 0; i < geusses.length; i++) {
       if (counter + space === geusses.length) {
@@ -177,14 +167,10 @@ window.onload = function() {
 
   // Play
   play = function() {
-    categories = [
-      ["everton", "liverpool", "swansea", "chelsea", "hull", "manchester-city", "newcastle-united"],
-      ["alien", "dirty-harry", "gladiator", "finding-nemo", "jaws"],
-      ["manchester", "milan", "madrid", "amsterdam", "prague"]
-    ];
+    categories = ["everton", "liverpool", "swansea", "chelsea", "hull", "manchester-city", "newcastle-united"];
+   
 
-    chosenCategory = categories[Math.floor(Math.random() * categories.length)];
-    word = chosenCategory[Math.floor(Math.random() * chosenCategory.length)];
+    word = categories[Math.floor(Math.random() * categories.length)];
     word = word.replace(/\s/g, "-");
     console.log(word);
     buttons();
@@ -195,7 +181,6 @@ window.onload = function() {
     space = 0;
     result();
     comments();
-    selectCat();
     canvas();
   }
 
@@ -205,15 +190,11 @@ window.onload = function() {
 
   hint.onclick = function() {
 
-    hints = [
-      ["Based in Mersyside", "Based in Mersyside", "First Welsh team to reach the Premier Leauge", "Owned by A russian Billionaire", "Once managed by Phil Brown", "2013 FA Cup runners up", "Gazza's first club"],
-      ["Science-Fiction horror film", "1971 American action film", "Historical drama", "Anamated Fish", "Giant great white shark"],
-      ["Northern city in the UK", "Home of AC and Inter", "Spanish capital", "Netherlands capital", "Czech Republic capital"]
-    ];
+    hints = ["Based in Mersyside", "Based in Mersyside", "First Welsh team to reach the Premier Leauge", "Owned by A russian Billionaire", "Once managed by Phil Brown", "2013 FA Cup runners up", "Gazza's first club"];
+  
 
-    var catagoryIndex = categories.indexOf(chosenCategory);
-    var hintIndex = chosenCategory.indexOf(word);
-    showClue.innerHTML = "Clue: - " + hints[catagoryIndex][hintIndex];
+    var hintIndex = categories.indexOf(word);
+    showClue.innerHTML = "Clue: - " + hints[hintIndex];
   };
 
   // Reset
